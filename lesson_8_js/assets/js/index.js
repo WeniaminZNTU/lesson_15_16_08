712d7571d05a94f83674a65dc3ecda75ff36a2a6
WeniaminZNTU <weniaminwelikii2015@gmail.com>
2020-08-08 15:04:33 +0300

    Initial commit

--- lesson_8_js/assets/js/index.js
+++ lesson_8_js/assets/js/index.js
@@ -1,2 +1,28 @@
 'use strict';
 
+let nam, quantity;
+let sharp = '#'
+
+quantity = +prompt('Enter the namber');
+
+if(quantity > 5000){
+    alert('Your namber is not correct');
+}
+
+console.log('------First cycle------')
+else if{while(nam <= quantity){
+    console.log(sharp);    
+}
+
+console.log('------Second cycle------')
+do{
+    console.log(sharp);
+}while(nam <= quantity);
+
+
+console.log('------Third cycle------')
+for(let i; i <= sharp; i++){
+    console.log(sharp);
+}
+
+}
